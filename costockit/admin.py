e2b745c061bd21479dc4f91b2c8d0aa2cf2e9730
b3j0f <jonathan.labejof@gmail.com>
2016-09-23 20:41:24 +0200

    :update: add models

--- costockit/admin.py
+++ costockit/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Account, Category, Stock, Product
+
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(Category)
+admin.site.register(Stock)
+admin.site.register(Product)
--- costockit/models.py
+++ costockit/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Account(models.Model):
+    user = models.ForeignKey(User)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+
+
+class Stock(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(Account)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(Account)
+    suppliers = models.ManyToManyField(Account, related_name='supplies')
+    categories = models.ForeignKey(Category)
+    stock = models.ForeignKey(Stock)
