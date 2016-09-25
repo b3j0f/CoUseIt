175741e4f19728568b08a15ab847c103876f5c21
b3j0f <jonathan.labejof@gmail.com>
2016-09-25 11:44:02 +0200

    :add: methods __str__ in the models

--- account_requests/models.py
+++ account_requests/models.py
@@ -6,15 +6,23 @@ from django.contrib.auth.models import User
 class CRUD(models.Model):
     name = models.CharField(max_length=6, primary_key=True)
 
+    def __str__(self):
+
+        return 'CRUD: {0}'.format(self.name)
+
 
 class StateRequest(models.Model):
-    name = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=11, primary_key=True)
+
+    def __str__(self):
+
+        return 'RequestState: {0}'.format(self.name)
 
 
 class AccountRequest(models.Model):
     """Permits to a user to ask to realize specific actions on the model."""
 
-    account = models.ForeignKey(User)
+    user = models.ForeignKey(User)
     creation = models.DateTimeField()
 
     action = models.ForeignKey(CRUD)
@@ -32,3 +40,7 @@ class AccountRequest(models.Model):
     comment = models.CharField(max_length=1024)
     answer = models.CharField(max_length=1024)
     answer_date = models.DateTimeField()
+
+    def __str__(self):
+
+        return 'Request(user: {0}, creation: {1}, action: {2}, state: {3}, comment: {4})'.format(self.user, self.creation, self.action, self.state, self.comment)
--- costockit/models.py
+++ costockit/models.py
@@ -5,16 +5,30 @@ from django.contrib.auth.models import User
 class Account(models.Model):
     user = models.ForeignKey(User)
 
+    def __str__(self):
+
+        return 'Account: {0}'.format(self.user)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
-    parent = models.ForeignKey('Category', related_name='children')
+    parent = models.ForeignKey(
+        'Category', related_name='children', default=None
+    )
+
+    def __str__(self):
+
+        return 'Category: {0}/{0}'.format(self.parent, self.name)
 
 
 class Stock(models.Model):
     name = models.CharField(max_length=50)
     owner = models.ForeignKey(Account)
-    parent = models.ForeignKey('Stock', related_name='children')
+    parent = models.ForeignKey('Stock', related_name='children', default=None)
+
+    def __str__(self):
+
+        return 'Stock: {0}/{1} ({2})'.format(self.parent, self.name, self.owner)
 
 
 class Product(models.Model):
@@ -23,3 +37,9 @@ class Product(models.Model):
     suppliers = models.ManyToManyField(Account, related_name='supplies')
     categories = models.ForeignKey(Category)
     stock = models.ForeignKey(Stock)
+
+    def __str__(self):
+
+        return 'Product: {0} {1}/{2} [{3}]'.format(
+            self.name, self.stock, self.owner, self.categories
+        )
